0b7f4fe0482bbd8feed47e2c0c155e1802efbe8d
wonth93 <wongth93@gmail.com>
2023-01-31 03:54:15 +0000

    complete exercise two

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,10 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+# find 1st and 2nd store by it's id
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+# Update the first store name from "Burnaby" to "Vancouver"
+@store1.update(name: "Vancouver")
